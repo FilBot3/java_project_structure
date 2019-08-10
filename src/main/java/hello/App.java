bdab6e8c7c4c0e4901339eae4b227eb2379b4286
Phillip Dudley <Predatorian3@gmail.com>
2019-08-10 10:29:11 -0500

    Got basic Java Ant and Gradle and Maven builds to work. Unable to run Jars though from the build system, so gotta figure that out.

--- src/main/java/hello/App.java
+++ src/main/java/hello/App.java
@@ -4,9 +4,20 @@
 package hello;
 
 /**
+ * App Class
+ * This class is the main launching point for the application.
  *
+ * @author Phillip Dudley 
+ * @version 0.1.0
+ * @since 0.1
  */
 public class App {
+  /**
+   * main
+   * This is the main function that is the driver for the class.
+   *
+   * @param args List of Strings from the command-line
+   */
   public static void main(String[] args) {
     System.out.println("Hello, World!");
   }
